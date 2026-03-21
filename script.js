01e483a01ae4365f13aef732e443183170b302f3
Milana <kilbovych.milana@gmail.com>
2026-03-21 16:25:10 +0200

    Add basic HTML structure and getComputerChoice function

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice(){
+
+    let randomNumber = Math.random();
+
+    if (randomNumber < 0.33) {
+        return("rock");
+    }
+    else if (randomNumber < 0.66) {
+        return("paper");
+    }
+    else{
+        return("scissors");
+    }
+}
+
+console.log(getComputerChoice());
